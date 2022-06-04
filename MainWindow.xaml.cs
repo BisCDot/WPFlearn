1c98a827d729cd9427c5d48e53398f10b61e4715
backk110 <nguyenbac0514@gmail.com>
2022-06-04 10:31:22 +0700

    new

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -54,5 +54,36 @@ namespace WpfLearn
                 PropertyChanged(this, new PropertyChangedEventArgs(newName));
             }
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click roi");
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Chua click");
+        }
+
+        private void cbAllFeatures_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbAllFeatures.IsChecked == true);
+            cbFeatureAbc.IsChecked = newVal;
+            cbFeatureXyz.IsChecked = newVal;
+            cbFeatureWww.IsChecked = newVal;
+        }
+
+        private void cbFeature_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            cbAllFeatures.IsChecked = null;
+            if ((cbFeatureAbc.IsChecked == true) && (cbFeatureXyz.IsChecked == true) && (cbFeatureWww.IsChecked == true))
+                cbAllFeatures.IsChecked = true;
+            if ((cbFeatureAbc.IsChecked == false) && (cbFeatureXyz.IsChecked == false) && (cbFeatureWww.IsChecked == false))
+                cbAllFeatures.IsChecked = false;
+        }
     }
 }
